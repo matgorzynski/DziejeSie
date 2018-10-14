cc962bbb8c64e1d73c7a06b84a5217c4f91160fb
Rafał <rafdab@st.amu.edu.pl>
2018-10-14 19:01:52 +0200

    Added models

---
+++ DziejeSieApp/DziejeSieApp/Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DziejeSieApp.Models
+{
+    public class Events
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Postcode { get; set; }
+        public string Town { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime AddDate { get; set; }
+        public Users User { get; set;}
+    }
+}
---
+++ DziejeSieApp/DziejeSieApp/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DziejeSieApp.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+    }
+}
