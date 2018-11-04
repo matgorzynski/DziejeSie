b55afcbf36eb699d656af20f6e26a0fb7aaefccb
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2018-11-04 21:59:12 +0100

    Creating Session for User

--- DziejeSieApp/DziejeSieApp/Startup.cs
+++ DziejeSieApp/DziejeSieApp/Startup.cs
@@ -18,18 +18,28 @@ namespace DziejeSieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          
+#if DEBUG
+            //dev database
+            //var connection = @"Server=mssql2.webio.pl,2401;Database=matgorzynski_DziejeSieApp_dev;Uid=matgorzynski_DziejeSieApp;Password=zaq1@WSX;";
+
+            //localhost database
+            var connection = @"Server=RAFCIO\DZIEJESIE;Database=dziejesie;Uid=dev;Password=dev;";
+#else
+            var connection = @"Server=mssql2.webio.pl,2401;Database=matgorzynski_DziejeSieApp;Uid=matgorzynski_DziejeSieApp;Password=zaq1@WSX;";
+#endif
 
             services.AddMvc()
                 .AddMvcOptions(o => o.OutputFormatters.Add(
                     new XmlDataContractSerializerOutputFormatter()));
-#if DEBUG
-            var connection = @"Server=mssql2.webio.pl,2401;Database=matgorzynski_DziejeSieApp_dev;Uid=matgorzynski_DziejeSieApp;Password=zaq1@WSX;";
-#else
-             var connection = @"Server=mssql2.webio.pl,2401;Database=matgorzynski_DziejeSieApp;Uid=matgorzynski_DziejeSieApp;Password=zaq1@WSX;";
-#endif
+
             services.AddDbContext<DziejeSieContext>
                 (options => options.UseSqlServer(connection));
+
+            services.AddSession(o =>
+            {
+                o.Cookie.Name = ".DziejeSie.Session";
+                o.IdleTimeout = TimeSpan.FromMinutes(600); //po 10 godzinach nieaktywności sesja po stronie serwera umiera [*]
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +50,7 @@ namespace DziejeSieApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSession(); //sesje
             app.UseMvc();
            
             app.Run(async (context) =>
