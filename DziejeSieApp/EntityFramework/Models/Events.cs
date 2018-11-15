36fafd26cba9dac285f3a9c42e4b3e1fbe80f773
Mateusz Górzyński <m.gorzynski96@gmail.com>
2018-11-15 20:35:58 +0100

    deleted unessary

--- DziejeSieApp/EntityFramework/Models/Events.cs
+++ DziejeSieApp/EntityFramework/Models/Events.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EntityFramework.Models
 {
-    
+
     public class Events
     {
         [Key]
