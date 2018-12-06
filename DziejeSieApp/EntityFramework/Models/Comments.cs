707dc6763e703491220a2e7007eb8ebde56ef280
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2018-12-06 19:59:53 +0100

    Commnet model and DB driver

---
+++ DziejeSieApp/EntityFramework/Models/Comments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFramework.Models
+{
+    public class Comments
+    {
+        [Key]
+        [Required]
+        public int CommentId { get; set; }
+
+        [Required]
+        public DateTime AddDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Body { get; set; }
+
+        [ForeignKey("UserId")]
+        public Users User { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("EventId")]
+        public Events Event { get; set; }
+        public int EventId { get; set; }
+    }
+}
