1fb6a2d01ef78674f8b333daececd308e982674c
Ma <lemateusz.gorzynski@hotmail.com>
2018-12-10 12:16:45 +0100

    start work with upvotes

---
+++ DziejeSieApp/EntityFramework/DBclass/Upvote.cs
@@ -0,0 +1,27 @@
+﻿using EntityFramework.DataBaseContext;
+using EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.DBclass
+{
+    class Upvote
+    {
+        private readonly DziejeSieContext _dbcontext;
+
+        public Upvote(DziejeSieContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public dynamic Positive (int UserId, int EventId)
+        {
+            try
+            {
+                Users User = new Users();
+                User 
+            }
+        }
+    }
+}
---
+++ DziejeSieApp/EntityFramework/Models/Upvotes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EntityFramework.Models
+{
+    class Upvotes
+    {
+        [Key]
+        [Required]
+        public int UpvoteId { get; set; }
+
+        [Required]
+        public int value { get; set; } // 1 or -1
+
+        [ForeignKey("UserId")]
+        [Required]
+        public Users User { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("EventId")]
+        [Required]
+        public Events Event { get; set; }
+        public int EventId{ get; set; }
+    }
+}
