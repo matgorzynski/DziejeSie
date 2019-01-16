c8dc11f75f1be2e5ef59a60f67bbc0f133cc2de7
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2019-01-16 15:22:15 +0100

    Upvotes controller

--- DziejeSieApp/DziejeSieApp/Controllers/UpvoteController.cs
+++ DziejeSieApp/DziejeSieApp/Controllers/UpvoteController.cs
@@ -14,22 +14,49 @@ namespace DziejeSieApp.Controllers
         [HttpPost]
         public JsonResult Plus(int UserId, int EventId)
         {
-            //return Json(new Upvote(_dbcontext).Positive(UserId, EventId));
-            
             string header = HttpContext.Request.Headers["VerySecureHeader"];
-            if (header != "")
-                if (LoggedIn.Contains(header))
+            if (header == "" || !LoggedIn.Contains(header))
+            {
+                var Error = new
                 {
-                    var Error = new
-                    {
-                        Code = 2,
-                        Type = "EventAdd",
-                        Desc = "User not logged in"
-                    };
+                    Code = 3,
+                    Type = "Upvote",
+                    Desc = "User not logged in"
+                };
 
-                    Response.StatusCode = 403; //Forbidden
-                    return Json(Error);
-                }
+                Response.StatusCode = 403; //Forbidden
+                return Json(Error);
+            }
+
+            return Json(new Upvote(_dbcontext).Positive(UserId, EventId));
+        }
+
+        [Route("upvote/minus")]
+        [HttpPost]
+        public JsonResult Minus(int UserId, int EventId)
+        {
+            string header = HttpContext.Request.Headers["VerySecureHeader"];
+            if (header == "" || !LoggedIn.Contains(header))
+            {
+                var Error = new
+                {
+                    Code = 3,
+                    Type = "Upvote",
+                    Desc = "User not logged in"
+                };
+
+                Response.StatusCode = 403; //Forbidden
+                return Json(Error);
+            }
+
+            return Json(new Upvote(_dbcontext).Negative(UserId, EventId));
+        }
+
+        [Route("upvote/points")]
+        [HttpPost]
+        public JsonResult Points(int EventId)
+        {
+            return Json(new Upvote(_dbcontext).Points(EventId));
         }
     }
 }
