c7b2ea83010229d3d3678e627865f3e977c6c3c7
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2019-01-17 13:48:15 +0100

    start wieth caontoller

---
+++ DziejeSieApp/DziejeSieApp/Controllers/CommentController.cs
@@ -0,0 +1,21 @@
+﻿using EntityFramework.DataBaseContext;
+using EntityFramework.DBclass;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DziejeSieApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly DziejeSieContext _dbcotext;
+
+        public CommentController(DziejeSieContext dbcontext) { _dbcotext = dbcontext; }
+
+        [Route("comments/add")]
+        [HttpPost]
+        public JsonResult AddComment([FromBody]Comment Comment)
+        {
+            string header = HttpContext.Request.Headers["VerySecureHeader"];
+            if(header == "" || !LoggedIn)
+        }
+    }
+}
